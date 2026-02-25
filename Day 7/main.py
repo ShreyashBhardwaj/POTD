0fb761dbe6749a8ee5bf151cd33df6c9db7a18f9
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2026-02-25 09:58:12 +0530

    Day 7 Start

---
+++ Day 7/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestSubarray(self, arr, k):
+        pass
+
+
+sol = Solution()
+
+print(sol.longestSubarray(arr = [1, 2, 3, 4, 1], k = 2))
+print(sol.longestSubarray(arr = [6, 5, 3, 4], k = 2))
