02c8f6d822d4199353ea592bafa7a67b9b76183b
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2026-03-01 11:08:26 +0530

    This code works but it is slower

--- Day 11/main.py
+++ Day 11/main.py
@@ -1,15 +1,26 @@
 class Solution:
     def pushZerosToEnd(self, arr):
-        n = len(arr)
-        ins_pos = 0
+        n = len(arr) -1
+        m = n
+        c=0
+        while n !=0 :
+            if arr[n]==0:
+                arr.pop(n)
+                c+=1
+                n=len(arr)-1
+            else:
+                n-=1
 
-        for i in range(n):
-            if arr[i] != 0:
-                arr[ins_pos] = arr[i]
-                ins_pos+=1
-
-        for i in range(ins_pos,n):
-            arr[i]=0
+        for i in range(c):
+           arr.append(0)
+        # ins_pos = 0
+        # for i in range(n):
+        #     if arr[i] != 0:
+        #         arr[ins_pos] = arr[i]
+        #         ins_pos+=1
+        #
+        # for i in range(ins_pos,n):
+        #     arr[i]=0
         return arr
 
 sol = Solution()
